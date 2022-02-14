1b77722af07a49d4bd94f98d91efb16a9716ebc7
sedric <kouamsedrick@gmail.com>
2022-02-14 13:41:29 -0800

    migration des tables en base de donnees

---
+++ database/migrations/2022_02_11_192929_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->integer('prix');
+            $table->foreignId('id_utilisateur')->constrained('Utilisateur')->cascadeOnDelete();
+            $table->foreignId('id_type_article')->constrained('TypeArticles')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
